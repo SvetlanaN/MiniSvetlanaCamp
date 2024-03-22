15b5c38eabd7b99d5edf096aa15d7225ba499915
Svetlana <svetlana.nikic@gmail.com>
2024-03-22 09:19:28 +0100

    Fix projects to work with start and end date

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,23 @@
 class Project < ApplicationRecord
+  before_create :set_timestamps
+  before_save :update_updated_at
+
   has_many :project_memberships
   has_many :users, through: :project_memberships
   has_many :tasks
 
   validates :name, presence: true
+  validates :description, presence: true
+
+  private
+
+  def set_timestamps
+    current_time = Time.current
+    self.created_at ||= current_time
+    self.updated_at ||= current_time
+  end
+
+  def update_updated_at
+    self.updated_at = Time.current
+  end
 end
